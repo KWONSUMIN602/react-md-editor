fd5cb08b9646735941b2371cadef928f285c1543
kwonsumin602 <92791424+KWONSUMIN602@users.noreply.github.com>
2023-07-07 15:05:53 +0900

    expot md file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,19 @@ const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
 function HomePage() {
   const [value, setValue] = useState("**Hello world!!!**");
 
+  const handleExport = () => {
+    const blob = new Blob([value], { type: "text/plain;charset=utf-8" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "markdown-file.md";
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <div>
+      <button onClick={handleExport}>Export</button>
       <MDEditor value={value} onChange={(value?: string) => setValue(value!)} />
     </div>
   );
